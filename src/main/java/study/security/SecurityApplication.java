abaa86e858c21db817680777b3a183fa276cd6d4
jjindol <andy15948@naver.com>
2023-08-09 14:32:31 +0900

    Feat: 수정 1

--- src/main/java/study/security/SecurityApplication.java
+++ src/main/java/study/security/SecurityApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication { // 코드 수정
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
-
 }
